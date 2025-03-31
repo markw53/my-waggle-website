254e0e7c55963979f325301edfadf455bae3b618
markw53 <m.workman@outlook.com>
2025-03-31 10:32:48 +0100

    files added

---
+++ src/app/config/theme.js
@@ -0,0 +1,27 @@
+// src/config/theme.js
+export const themes = {
+    light: {
+      colors: {
+        primary: '#0070f3',
+        background: '#ffffff',
+        text: '#000000',
+        // Add other colors as needed
+      },
+      shadows: {
+        small: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+        // Add other shadow definitions
+      }
+    },
+    dark: {
+      colors: {
+        primary: '#0070f3',
+        background: '#121212',
+        text: '#ffffff',
+        // Add other colors as needed
+      },
+      shadows: {
+        small: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+        // Add other shadow definitions
+      }
+    }
+  };
